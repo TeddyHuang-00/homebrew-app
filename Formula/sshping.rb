4838702174a30be04ce48a525fccdb93b2eb9f43
Nan Huang <teddyhuangnan@gmail.com>
2025-02-08 00:09:44 -0500

    feat: Update version and install completion files

--- Formula/sshping.rb
+++ Formula/sshping.rb
@@ -1,17 +1,21 @@
 class Sshping < Formula
   desc "SSH-based ping that measures echo latency and transfer speed"
   homepage "https://crates.io/crates/sshping"
-  version = "0.1.4"
+  version = "0.2.0"
 
   url "https://github.com/TeddyHuang-00/sshping/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "a79c1ba4a6de58fd93e67010a2d15e4dedf84a2e85732a03b1c2acdb018a4f3c"
+  sha256 "6d1b4ddc5f39cadb9c9f2e8f9dd81db3555d456785a3707e595761ce91b2e474"
   license "MIT"
 
   depends_on "rust" => :build
   depends_on "openssl"
 
   def install
-    system "cargo", "install", *std_cargo_args
+    system "cargo", "build", "-r"
+    bin.install "target/release/sshping"
+    bash_completion.install "target/release/completions/sshping.bash"
+    zsh_completion.install "target/release/completions/_sshping"
+    fish_completion.install "target/release/completions/sshping.fish"
   end
 
   test do
