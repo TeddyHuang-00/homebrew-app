a77937d3c16069ebd26e31987db2b4071acf68b8
Nan Huang <teddyhuangnan@gmail.com>
2025-12-18 13:45:11 +0900

    fix: Fix url order

--- Formula/sshping.rb
+++ Formula/sshping.rb
@@ -1,6 +1,10 @@
 class Sshping < Formula
   desc "SSH-based ping that measures echo latency and transfer speed"
   homepage "https://github.com/TeddyHuang-00/sshping"
+  version = "0.2.3"
+  url "https://github.com/TeddyHuang-00/sshping/archive/refs/tags/v#{version}.tar.gz"
+  sha256 "3e65064c87edab29a6f0253cacfad3f423728ea70f5d24259e61a75ba68fff61"
+  license "MIT"
 
   bottle do
     root_url "https://github.com/TeddyHuang-00/homebrew-app/releases/download/sshping-0.2.2"
@@ -8,11 +12,6 @@ class Sshping < Formula
     sha256 cellar: :any,                 ventura:       "3e65064c87edab29a6f0253cacfad3f423728ea70f5d24259e61a75ba68fff61"
     sha256 cellar: :any_skip_relocation, x86_64_linux:  "3e65064c87edab29a6f0253cacfad3f423728ea70f5d24259e61a75ba68fff61"
   end
-  version = "0.2.3"
-
-  url "https://github.com/TeddyHuang-00/sshping/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "3e65064c87edab29a6f0253cacfad3f423728ea70f5d24259e61a75ba68fff61"
-  license "MIT"
 
   depends_on "rust" => :build
   depends_on "openssl@3"
