37de0cfadd316d038b16f49e6720a54f45e341a1
Nan Huang <teddyhuangnan@gmail.com>
2025-12-18 13:38:37 +0900

    Revert "chore: Update sshping to 0.2.3"
    This reverts commit d17f57a9774f588a184d88cf0341f9600b69cbec.

--- Formula/sshping.rb
+++ Formula/sshping.rb
@@ -4,14 +4,14 @@ class Sshping < Formula
 
   bottle do
     root_url "https://github.com/TeddyHuang-00/homebrew-app/releases/download/sshping-0.2.2"
-    sha256 cellar: :any,                 arm64_sequoia: "3e65064c87edab29a6f0253cacfad3f423728ea70f5d24259e61a75ba68fff61"
-    sha256 cellar: :any,                 ventura:       "3e65064c87edab29a6f0253cacfad3f423728ea70f5d24259e61a75ba68fff61"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3e65064c87edab29a6f0253cacfad3f423728ea70f5d24259e61a75ba68fff61"
+    sha256 cellar: :any,                 arm64_sequoia: "c2de9e5c66b7e273c583f7e431cdd9c9326fa7269fb5352901408554cddc4fa2"
+    sha256 cellar: :any,                 ventura:       "ff09e1dce98f8265e5b956b9c33603f26b436fa3ffa33bb914d742b9f631a41b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "3d8c259f6214182cf30bab44613928da86dd25c022c2b3175f8f81f39c593bce"
   end
-  version = "0.2.3"
+  version = "0.2.2"
 
   url "https://github.com/TeddyHuang-00/sshping/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "3e65064c87edab29a6f0253cacfad3f423728ea70f5d24259e61a75ba68fff61"
+  sha256 "03dc3db16fce47512c20586a7b2dff8d0cff255348a964b8bfc6dd4ddca73501"
   license "MIT"
 
   depends_on "rust" => :build
