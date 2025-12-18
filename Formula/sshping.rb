b22413ec907b15b58b8585b394039c8ad4fcac69
Nan Huang <teddyhuangnan@gmail.com>
2025-12-18 06:41:27 +0000

    sshping: add 0.2.3 bottle.

--- Formula/sshping.rb
+++ Formula/sshping.rb
@@ -1,6 +1,12 @@
 class Sshping < Formula
   desc "SSH-based ping that measures echo latency and transfer speed"
   homepage "https://github.com/TeddyHuang-00/sshping"
+
+  bottle do
+    root_url "https://github.com/TeddyHuang-00/homebrew-app/releases/download/sshping-0.2.3"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "49644932409bffc590c3f52932fd44a1c06fdee49de84dea0a6fd727d859befe"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "69fb9317b4c045463b137b98ecc6140f3ffac3cd1b1d89df733118ffe1ccca1a"
+  end
   version = "0.2.3"
   url "https://github.com/TeddyHuang-00/sshping/archive/refs/tags/v#{version}.tar.gz"
   sha256 "0a4cc419f796176fa0396985ce74389ce3171d9ab8a581f1388e1d524d48b681"
