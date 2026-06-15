574b5f3b37026d609fe1d517b83f21107981d695
Nan Huang <teddyhuangnan@gmail.com>
2026-06-15 00:22:47 -0400

    chore: update sshping to 0.4.1
    Closes #6.
    
    Signed-off-by: Nan Huang <teddyhuangnan@gmail.com>

--- Formula/sshping.rb
+++ Formula/sshping.rb
@@ -1,18 +1,9 @@
 class Sshping < Formula
   desc "SSH-based ping that measures echo latency and transfer speed"
   homepage "https://github.com/TeddyHuang-00/sshping"
-
-  bottle do
-    root_url "https://github.com/TeddyHuang-00/homebrew-app/releases/download/sshping-0.3.2"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "4a712ec5a0edbca5702ad261dd5a1af35ecd1656f9a9eaab0fa6a36b8850e5ee"
-    sha256 cellar: :any,                 arm64_linux:  "27b3251ecfa616811671501104f75e00117351f4005eeefcc190d47b62c8c850"
-    sha256 cellar: :any,                 x86_64_linux: "fa08744c2aff70f1b0746a7ee046a7399a45fbb5479bc5f661aa540e7d45966c"
-  end
-
-  version = "0.3.2"
-
+  version = "0.4.1"
   url "https://github.com/TeddyHuang-00/sshping/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "13d739dc66794dff584fa4f1d736b633761b2a72bdafec450607923e2014d828"
+  sha256 "cd69f1ef2ff779a2ff9f96af3da9f4b8ee7c065ab2894c5eda42313fa96bc97e"
   license "MIT"
   depends_on "rust" => :build
   depends_on "openssl@3"
